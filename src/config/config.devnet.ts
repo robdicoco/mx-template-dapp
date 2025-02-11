a1ddd68ac3323a1ae8b44052e10a649873595b8c
robdicoco <pavusa@gmail.com>
2025-02-10 22:10:42 -0300

    dapp changes

--- src/config/config.devnet.ts
+++ src/config/config.devnet.ts
@@ -3,7 +3,7 @@ import { EnvironmentsEnum } from 'types';
 export * from './sharedConfig';
 
 export const contractAddress =
-  'erd1qqqqqqqqqqqqqpgqm6ad6xrsjvxlcdcffqe8w58trpec09ug9l5qde96pq';
+  'erd1qqqqqqqqqqqqqpgqljej56j6vrcru7tq6agctevwh39uupwcwa8ssksywy';
 export const API_URL = 'https://devnet-template-api.multiversx.com';
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.devnet;
